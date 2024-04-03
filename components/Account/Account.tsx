0e23412066b96b893ebe256d3a9e347cacca9fa3
cubecubb <annazibereva999@gmail.com>
2024-04-03 19:32:34 +0700

    фикса

--- components/Account/Account.tsx
+++ components/Account/Account.tsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import { View } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../Navigate";
+import СurrentGradesModulesStudent from "./PersonalAccountStudent/FunctionalModulesStudent/СurrentGradesModulesStudents/СurrentGradesModulesStudent";
 type ScheduleProps = {
   navigation: StackNavigationProp<RootStackParamList, "Schedule">;
 };
@@ -24,6 +25,7 @@ const Account = ({ navigation }: ScheduleProps) => {
         <Authorization navigation={navigation}/>
       ) : (
         <View>
+          <СurrentGradesModulesStudent/>
         </View>
       )}
     </Container>
